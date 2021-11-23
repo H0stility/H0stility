65fc680ab4bb5d9c48ba847f6ce8a59347d17637
H0stility <91197977+H0stility@users.noreply.github.com>
2021-11-23 16:50:16 +0100

    Add files via upload

---
+++ Projet NoDebt/html/Accueil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/Page1.css">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <?php include("inc/header.inc.php");?>
+    <div class="ensemble">
+        <div class="titre">
+            Accueil
+        </div>
+        <div class="formulaire">
+            <div class="champ_input">
+                <a href="Connexion.html" class="boutton">Connexion</a>
+            </div>
+            <div class="champ_input">
+                <a href="Inscription.html" class="boutton">S'inscrire</a>
+            </div>
+            <div class="champ_input">
+                <a href="Contact.html" class="boutton">Un problème ? Contactez-nous.</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Projet NoDebt/html/inc/header.inc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/Page1.css">
+
+</head>
+
+<body class="body_header">
+    <header class="header">
+        <h1 class="titre_header">NoDebt</h1>
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <a href="#" class="cta"><button class="bouton3">Contact</button></a>
+    </header>
+</body>
+
+</html>
